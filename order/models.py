7baad5c9885491baa4e577bb426a0d0df726e575
mkhalil7625@gmail.com <mkhalil7625@gmail.com>
2019-05-15 14:40:00 -0500

    remove test view

--- order/models.py
+++ order/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Order(models.Model):
     token = models.CharField(max_length=250, blank=True)
-    total = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='GBP Order Total')
+    total = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='USD Order Total')
     emailAddress = models.EmailField(max_length=250, blank=True, verbose_name='Email Address')
     created = models.DateTimeField(auto_now_add=True)
     billingName = models.CharField(max_length=250, blank=True)
--- order/urls.py
+++ order/urls.py
@@ -5,6 +5,4 @@ app_name='order'
 
 urlpatterns = [
 	path('order_confirmation/<int:order_id>/', views.order_confirmation, name='order_confirmation'),
-	# path('history/', views.orderHistory, name='order_history'),
-	# path('<int:order_id>/', views.viewOrder, name='order_detail'),
 ]
